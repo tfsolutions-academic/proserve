108a2694644ea4be8fc9e45e31e1ba838b9a814d
Natarajan <Teamforward.in@gmail.com>
2021-03-01 16:36:43 +0530

    Generic Data Generator

--- UI/pages/generator.js
+++ UI/pages/generator.js
@@ -7,124 +7,104 @@ import { InputText } from 'primereact/inputtext';
 import { Panel } from 'primereact/panel';
 import { Button } from "primereact/button";
 import { Badge } from 'primereact/badge';
-
 import AppContext from '../utils/appContext';
 
-
-const generateVenues = (count) => {
-
-    let venues = [];
-
-    for(let i=0;i<=count-1;i++){
-        venues.push(
-            {
-                "name": faker.company.companyName(),
-                "capacity": faker.random.number({min :1500, max: 5000}),
-                "address": {
-                  "address1": faker.address.streetName(),
-                  "address2": faker.address.secondaryAddress(),
-                  "city": faker.address.city(),
-                  "state": faker.address.stateAbbr(),
-                  "pincode": faker.address.zipCode()
-                },
-                "amenties": {
-                  "CarParking": faker.random.boolean(),
-                  "Restrooms": true,
-                  "AirConditioning": faker.random.boolean(),
-                  "Kitchen": faker.random.boolean(),
-                  "Furnitures": faker.random.boolean()
-                },
-                "Contacts": [
-                  {
-                    "name": faker.name.firstName(faker.random.number({min:1,max:2})),
-                    "number": faker.phone.phoneNumber(),
-                    "whatsapp": faker.random.boolean(),
-                    "primary": true
-                  }
-                ],
-                "published_at": new Date().toISOString(),
-                "created_by": "proserve admin",
-                "updated_by": "proserve admin"
-              }
-        );
-    }
-
-    return venues;
+const venueEntity = {
+    "name": faker.company.companyName(),
+    "capacity": faker.random.number({min :1500, max: 5000}),
+    "address": {
+        "address1": faker.address.streetName(),
+        "address2": faker.address.secondaryAddress(),
+        "city": faker.address.city(),
+        "state": faker.address.stateAbbr(),
+        "pincode": faker.address.zipCode()
+    },
+    "amenties": {
+        "CarParking": faker.random.boolean(),
+        "Restrooms": true,
+        "AirConditioning": faker.random.boolean(),
+        "Kitchen": faker.random.boolean(),
+        "Furnitures": faker.random.boolean()
+    },
+    "Contacts": [
+        {
+        "name": faker.name.firstName(faker.random.number({min:1,max:2})),
+        "number": faker.phone.phoneNumber(),
+        "whatsapp": faker.random.boolean(),
+        "primary": true
+        }
+    ],
+    "published_at": new Date().toISOString(),
+    "created_by": "proserve admin",
+    "updated_by": "proserve admin"
 }
 
-const generateOrgainizers = (count) => {
+const orgainizerEntity = {
+    "name": faker.name.firstName(),
+    "contact": [
+      {
+        "name": faker.name.lastName(),
+        "number": faker.phone.phoneNumber(),
+        "whatsapp": faker.random.boolean(),
+        "primary": faker.random.boolean()
+      }
+    ],
+    "published_at": new Date().toISOString(),
+    "created_by": "proserve admin",
+    "updated_by": "proserve admin"
+}
 
-    let orgs = [];
+const EntityGenerator = (props) => {
+    const [index, setIndex] = useState(0);
+    const [count,setCount] = useState(10);
+    const [status,setStatus] = useState({success : 0, failed : 0});
 
-    for(let i=0;i<=count-1;i++){
-        orgs.push(
-            {
-                "name": faker.name.firstName(),
-                "contact": [
-                  {
-                    "name": faker.name.lastName(),
-                    "number": faker.phone.phoneNumber(),
-                    "whatsapp": faker.random.boolean(),
-                    "primary": faker.random.boolean()
-                  }
-                ],
-                "published_at": new Date().toISOString(),
-                "created_by": "proserve admin",
-                "updated_by": "proserve admin"
-              }
-        );
+    const onGenerateData = async () => {
+        const postData = async (data) => {
+            return await props.service.post(props.endPoint,data); 
+        }
+        setStatus({success : 0, failed : 0});
+        setIndex(0);
+        let successCnt = 0;
+        let failedCnt = 0;
+        for(let i=0;i<=count-1;i++){
+            try{
+                let entity = props.entity;
+                await postData(entity);
+                successCnt++;
+            }catch(er){
+                failedCnt++;
+                console.log(er);
+            }
+            setIndex(i+1);
+        }
+        setStatus({success : successCnt, failed : failedCnt});
     }
 
-    return orgs;
+    return(
+        <Card header={props.header}>
+            <div className="p-inputgroup">
+                <InputText value={count} onChange={(e) => setCount(e.target.value)} />
+                <Button icon="pi pi-check" label="Generate" onClick={onGenerateData} className="p-button-success"/>
+            </div>
+            <div style={{"display":"flex","flexDirection":"row","alignItems":"center","justifyContent":"space-evenly"}}>
+                <Knob value={index} min={0} max={count} /> 
+                <div>
+                        <Button type="button" label="Success">
+                            <Badge value={status.success} severity="success"/>
+                        </Button>
+                        <hr />
+                        <Button type="button" label="Failed">
+                            <Badge value={status.failed} severity="danger"/>
+                        </Button>
+                </div>
+            </div>
+        </Card> 
+    );
 }
 
 const DataGenerator = (props) => {
     const appContext = useContext(AppContext);
-    const [venueCount,setVenueCount] = useState(50);
-    const [venueStatus,setVenueStatus] = useState({success : 0, failed : 0});
-    const [orgCount,setOrgCount] = useState(50);
-    const [orgStatus,setOrgStatus] = useState({success : 0, failed : 0});
-    const [index, setIndex] = useState(0);
-
-    const onGenerateVenues = () => {
-        const postVenue = async (data) => {
-            return await appContext.axiosInstance.post('/venues',data); 
-        }
-        setVenueStatus({success : 0, failed : 0});
-        setIndex(0);
-        let successCnt = 0;
-        let failedCnt = 0;
-        generateVenues(+venueCount).map((venue,index) => {
-            try{
-                postVenue(venue);
-                successCnt++;
-            }catch(er){
-                failedCnt++;
-            }
-            setIndex(index+1);
-        });
-        setVenueStatus({success : successCnt, failed : failedCnt});
-    }
-
-    const onGenerateOrgainizers = () => {
-        const postOrg = async (data) => {
-            return await appContext.axiosInstance.post('/orgainizers',data); 
-        }
-        setOrgStatus({success : 0, failed : 0});
-        setIndex(0);
-        let successCnt = 0;
-        let failedCnt = 0;
-        generateOrgainizers(+orgCount).map((org,index) => {
-            try{
-                postOrg(org);
-                successCnt++;
-            }catch(er){
-                failedCnt++;
-            }
-            setIndex(index+1);
-        });
-        setOrgStatus({success : successCnt, failed : failedCnt});
-    }
 
     return(
         <Card style={{height:'100vh', backgroundColor:'ivory'}}>
@@ -135,45 +115,11 @@ const DataGenerator = (props) => {
             <Panel header="Generate">
                 <div className="p-grid p-fluid">
                     <div className="p-col-12 p-md-4">
-                        <Card header="Venues">
-                            <div className="p-inputgroup">
-                                <InputText value={venueCount} onChange={(e) => setVenueCount(e.target.value)} />
-                                <Button icon="pi pi-check" label="Generate" onClick={onGenerateVenues} className="p-button-success"/>
-                            </div>
-                            <div style={{"display":"flex","flexDirection":"row","alignItems":"center","justifyContent":"space-evenly"}}>
-                                <Knob value={index} min={0} max={venueCount} /> 
-                                <div>
-                                      <Button type="button" label="Success">
-                                            <Badge value={venueStatus.success} severity="success"/>
-                                      </Button>
-                                      <hr />
-                                      <Button type="button" label="Failed">
-                                            <Badge value={venueStatus.failed} severity="danger"/>
-                                      </Button>
-                                </div>
-                            </div>
-                        </Card>                        
+                        <EntityGenerator header="Venues" service={appContext.axiosInstance} entity={venueEntity} endPoint="/venues" />
                     </div>
 
                     <div className="p-col-12 p-md-4">
-                        <Card header="Orgainizers">
-                            <div className="p-inputgroup">
-                                <InputText value={orgCount} onChange={(e) => setOrgCount(e.target.value)} />
-                                <Button icon="pi pi-check" label="Generate" onClick={onGenerateOrgainizers} className="p-button-success"/>
-                            </div>
-                            <div style={{"display":"flex","flexDirection":"row","alignItems":"center","justifyContent":"space-evenly"}}>
-                                <Knob value={index} min={0} max={orgCount} /> 
-                                <div>
-                                    <Button type="button" label="Success">
-                                        <Badge value={orgStatus.success} severity="success"/>
-                                    </Button>
-                                    <hr />
-                                    <Button type="button" label="Failed">
-                                        <Badge value={orgStatus.failed} severity="danger"/>
-                                    </Button>
-                                </div>
-                            </div>
-                        </Card> 
+                        <EntityGenerator header="Orgainizers" service={appContext.axiosInstance} entity={orgainizerEntity} endPoint="/orgainizers" />
                     </div>
 
                     <div className="p-col-12 p-md-4">
